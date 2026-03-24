13afd04f6a5e83e5707fb091ae50484ea6ba19c0
Jonathan Milhas <jonathan.milhas@ihu-liryc.fr>
2026-03-24 16:09:53 +0100

    refacto: remove sql injection migrate to gorm

--- backend/internal/db/migrations/003_ecg_hl7_retry_count.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE ecgs ADD COLUMN hl7_retry_count INT NOT NULL DEFAULT 0;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE ecgs DROP COLUMN hl7_retry_count;
--- +goose StatementEnd
--- backend/internal/db/migrations/004_ecg_recorded_at.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE ecgs ADD COLUMN recorded_at TIMESTAMPTZ;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE ecgs DROP COLUMN recorded_at;
--- +goose StatementEnd
